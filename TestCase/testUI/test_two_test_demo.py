6fb74c2191c9e4f58b54774b382324594c51f17c
任长江 <16621220671@163.com>
2019-04-03 11:49:57 +0800

    14

--- TestCase/testUI/test_two_test_demo.py
+++ TestCase/testUI/test_two_test_demo.py
@@ -15,10 +15,6 @@ class Test_first_test_demo:
         base.send_keys('输入密码', "//input[@name='password']", '123456')
         base.click('登陆', "(//span[contains(text(),'登录')])[1]")
         base.click('点击商品',"//span[contains(text(),'商品')]")
-        base.click('测试',"//span[contains(text(),'添加商品')]")
-        base.click('测试',"(//label[contains(text(),'商品分类：')]/following-sibling::div//span)[1]")
-        base.click('测试',"//li[contains(text(),'手机数码')]")
-        base.click('测试', "//li[contains(text(),'手机配件')]")
         base.send_keys('测试', "//label[contains(text(),'商品名称：')]/following-sibling::div//input", '耳机')
         base.send_keys('测试', "//label[contains(text(),'副标题：')]/following-sibling::div//input", '家hhhh用')
         base.click('测试', "//label[contains(text(),'商品品牌：')]/following-sibling::div//input")
@@ -26,16 +22,37 @@ class Test_first_test_demo:
 
         js = "var q=document.documentElement.scrollTop=10000"
         driver.execute_script(js)
-        time.sleep(1)
+        time.sleep(2)
 
-        base.click('测试', "//button/span[contains(text(),'下一步，填写商品促销')]")
+        base.click('测试', "//span[contains(text(),'下一步，填写商品促销')]")
+        base.click('测试', "//span[contains(text(),'下一步，填写商品属性')]")
 
-        base.click('测试', " // span[text() = '下一步，填写商品属性']")
 
-        base.click('测试', " // span[text() = '下一步，选择商品关联']")
+        base.click('属性类型', "//label[contains(text(), '属性类型：')] /following-sibling::div/div/div")
+        base.click('配件', "//span[text()='配件']")
+        base.click('电脑详情', "(// label[contains(text(), '规格参数：')] /following-sibling::div/div/div/div/div/div/div)[1]")
+        xpath = driver.find_element_by_xpath("(//iframe[contains(@id,'vue-tinymce')])[1]")
+        driver.switch_to_frame(xpath)
+        # 填写规格参数
+        base.send_keys("填写规格参数", "//body[@id='tinymce']", "一桌晶")
+        driver.switch_to_default_content()
+
+        base.click('下一步,选择商品关联', "//span[contains(text(),'下一步，选择商品关联')]")
+
+
+
+
+
+
+
+
+
+        # base.click('测试', " // span[text() = '下一步，填写商品属性']")
         #
-        base.click('测试', " // span[text() = '完成，提交商品']")
-        #
+        # base.click('测试', " // span[text() = '下一步，选择商品关联']")
+        # #
+        # base.click('测试', " // span[text() = '完成，提交商品']")
+        # #
         # base.click('测试', " // span[text() = '完成，提交商品']")
         #
         # base.click('测试', "//span[contains(text(),'   确定')]")
